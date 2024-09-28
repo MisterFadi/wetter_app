5c8663795daccfca1b681301806ea51739391918
Fadi Bdiwi <fadibdiwi98@yahoo.de>
2024-09-28 19:08:00 +0200

    add Scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
-    );
+    return Scaffold(
+        appBar: AppBar(
+      title: const Text("Wetter-App"),
+    ));
   }
 }
